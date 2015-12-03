b910ea17230f5ac380c06ee548bdf64d1fa6dde0
steelbrain <aneesiqbalbhatti@gmail.com>
2015-12-03 09:45:55 -0700

    :new: Add main file

---
+++ src/main.js
@@ -0,0 +1,17 @@
+'use babel'
+
+import Cluster from 'cluster'
+import ChildProcess from 'child_process'
+import {Communication} from './communication'
+
+export function current() {
+  return new Communication(process)
+}
+
+export function fork(env) {
+  return new Communication(Cluster.fork(env || process.env))
+}
+
+export function forkFile(filePath) {
+  return new Communication(ChildProcess.fork(filePath))
+}
